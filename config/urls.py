dfa913f2a6127c2ae58205c17adba1ac1cfb9985
raph2611toky <raphaeltokinandrasana@gmail.com>
2025-07-21 09:26:59 +0300

    first commit

---
+++ config/urls.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+from django.conf import settings
+from django.conf.urls.static import static
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Serveur de Streaming Video",
+        default_version='v1',
+        description="API pour un serveur de streaming",
+        contact=openapi.Contact(email="contact@monapi.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('apps.users.urls')),
+    
+    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
